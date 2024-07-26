d658894984411a706cd2ffe6e48629920658b806
Suthar Mahesh M <125433167+sutharmahesh05@users.noreply.github.com>
2024-07-26 22:16:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ document.getElementById('contact-form').addEventListener('submit', function(e) {
     e.preventDefault();
 
     const name = document.getElementById('name').value;
+    const name = document.getElementById('Mobile').value;
+    const name = document.getElementById('Address').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
 
-    alert(`Thank you, ${name}. Your message has been sent!`);
+    alert(`Thank you, ${name}. Your message has been sent! & give u respsonse as soon as possible`);
 
     // Optionally, add code to actually send the form data to a server here
 });
